7df83e11aa0bba584792006c4c5ad23eebde049c
San-Rog <118987989+San-Rog@users.noreply.github.com>
2025-05-18 18:23:04 -0300

    Update localStorage.py

--- localStorage.py
+++ localStorage.py
@@ -1,14 +1,15 @@
 import streamlit as st
 from streamlit_local_storage import LocalStorage
 
-localS = LocalStorage()
+localStorage = LocalStorage()
 localS.deleteAll()
 
 st.info('Encontrado')
 st.write(localS.getAll())
 
 st.info('enchendo...')
-localS.setItem('mama', 'ddddd')
-#localS.setItem('lema', 'seis')
+localStorage.setItem("nome", "João");
+localStorage.setItem("idade", "30");
+localStorage.setItem("cidade", "São Paulo");
 st.write(localS.getAll())
 
