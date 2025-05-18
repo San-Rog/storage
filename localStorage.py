463111fcf034cebf1baf4aa67f5fbf16c0d1430c
San-Rog <118987989+San-Rog@users.noreply.github.com>
2025-05-18 17:23:09 -0300

    Add files via upload

---
+++ localStorage.py
@@ -0,0 +1,30 @@
+import streamlit as st
+from streamlit_local_storage import LocalStorage
+localS = LocalStorage()
+
+localS.deleteAll()
+
+#Gravar o para chave-valor em localstorage
+for categ, pairs in {'dados': ['Zebra', 'tratmento', 'tubo', 'martelo']}.items():    
+    itemKey = categ
+    itemValue = pairs
+    st.write(itemKey)
+    st.write(itemValue)
+    st.write(localS.getAll())
+    localS.setItem(itemKey, itemValue)
+st.success("Gravação exitosa")
+
+#Ler a chave camera.
+#itemKey = "camera"
+#storageItem = localS.getItem(itemKey)
+#st.info(storageItem)
+
+#Ler todos os pares chae-valor em LocalStorage
+#allItens = localS.getAll()
+#st.write(allItens)
+
+#Apagar tudo em localStorage
+#localS.deleteAll()
+
+#allItens = localS.getAll()
+#st.write(allItens)
