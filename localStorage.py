109b7a16f99bb313d27e4b89714b9adf8958f3d7
San-Rog <118987989+San-Rog@users.noreply.github.com>
2025-05-18 18:16:13 -0300

    Update localStorage.py

--- localStorage.py
+++ localStorage.py
@@ -7,34 +7,6 @@ st.info('Encontrado')
 st.write(localS.getAll())
 
 st.info('enchendo...')
-
-def supply(itemKey, itemValue):
-    st.write(itemKey)
-    st.write(itemValue)
-    localS.setItem(itemKey, itemValue)
-st.success("Gravação exitosa")
-
-itemKey = 'dados' 
-itemValue = ['Zebra', 'tratmento', 'tubo', 'martelo']
-supply(itemKey, itemValue)
-itemKey = 'zebra' 
-itemValue = ['carrro', 0, 12, 14]
-supply(itemKey, itemValue)
-st.write(localS.getAll())
-
-#Ler a chave camera.
-#itemKey = "camera"
-#storageItem = localS.getItem(itemKey)
-#st.info(storageItem)
-
-#Ler todos os pares chae-valor em LocalStorage
-#allItens = localS.getAll()
-#st.write(allItens)
-
-#Apagar tudo em localStorage
-#localS.deleteAll()
-
-#allItens = localS.getAll()
-#st.write(allItens)
-
+localS.setItem('mama', 10)
+localS.setItem('lema', 20)
 
