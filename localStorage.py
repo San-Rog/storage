4dc59f9c1c41d863d9501195a075b9e3317097cf
San-Rog <118987989+San-Rog@users.noreply.github.com>
2026-07-04 11:53:29 -0300

    Update localStorage.py

--- localStorage.py
+++ localStorage.py
@@ -1,31 +1,4 @@
 import streamlit as st
-from streamlit_local_storage import LocalStorage
-
-localStorage = LocalStorage()
-st.write(localStorage.getAll())
-#localStorage.deleteAll()
-
-st.info('Encontrado')
-st.write(localStorage.getAll())
-
-st.info('enchendo...')
-localStorage.setItem("dados", [[12, 3, 4, 5, 6, 7], [2, 4, 5],
-                               [0], [2222]])
-st.write(localStorage.getAll())
-
-import pypickle
-#Arquivo pickle
-filepath = 'test.pkl'
-
-# Some data
-data = [1,2,3,4,5]
-
-# Save
-status = pypickle.save(filepath, data)
-
-# Load file
-data = pypickle.load(filepath)
-st.write(data)
 
 value = st.slider(label="Select a range of values", 
                   min_value=-100, 
